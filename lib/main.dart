aa0e56dd71886c20f534e51f881213d37bdb734f
Phillip LeMaster <philalem21@gmail.com>
2020-07-01 15:01:14 -0400

    added the buttom button bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Noted',
+      home: MyHomePage(title: 'Create a note'),
     );
   }
 }
@@ -27,28 +27,124 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    FocusNode focusNode = FocusNode();
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        middle: Text(widget.title),
+        middle: Text(
+          widget.title,
+          style: TextStyle(color: Colors.black),
+        ),
         brightness: Brightness.light,
         border: Border(bottom: BorderSide(color: Colors.transparent)),
         backgroundColor: Colors.transparent,
+        trailing: CupertinoButton(
+          onPressed: () {
+            !focusNode.hasFocus
+                ? SystemChannels.textInput.invokeMethod('TextInput.hide')
+                : focusNode.unfocus();
+          },
+          padding: EdgeInsets.all(0.0),
+          child: Text(
+            'Done',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
       ),
       backgroundColor: Colors.grey[100],
       child: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          child: ListView(
-            children: <Widget>[
-              CupertinoTextField(
-                decoration: BoxDecoration(color: Colors.transparent),
-                textCapitalization: TextCapitalization.sentences,
-                placeholder: "Hello, World!",
-                placeholderStyle: TextStyle(color: Colors.red),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: GestureDetector(
+                onTap: () => focusNode.requestFocus(),
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: CupertinoTextField(
+                        autofocus: true,
+                        focusNode: focusNode,
+                        onTap: () => focusNode.requestFocus(),
+                        maxLines: null,
+                        decoration: BoxDecoration(color: Colors.transparent),
+                        textCapitalization: TextCapitalization.sentences,
+                        placeholder: "Write something...",
+                        style: TextStyle(color: Colors.black),
+                        placeholderStyle: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
+            ),
+            textEditingRow()
+          ],
         ),
+      ),
+    );
+  }
+
+  Container textEditingRow() {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: CupertinoButton(
+              onPressed: () => print('pressed B'),
+              color: Colors.grey[200],
+              padding: EdgeInsets.all(0),
+              child: Text(
+                'B',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CupertinoButton(
+              onPressed: () => print('pressed I'),
+              padding: EdgeInsets.all(0),
+              color: Colors.grey[200],
+              child: Text(
+                'I',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CupertinoButton(
+              onPressed: () => print('pressed U'),
+              padding: EdgeInsets.all(0),
+              color: Colors.grey[200],
+              child: Text(
+                'U',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CupertinoButton(
+              onPressed: () => print('pressed A^'),
+              padding: EdgeInsets.all(0),
+              color: Colors.grey[200],
+              child: Text(
+                'A^',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CupertinoButton(
+              onPressed: () => print('pressed a^'),
+              padding: EdgeInsets.all(0),
+              color: Colors.grey[200],
+              child: Text(
+                'a^',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
